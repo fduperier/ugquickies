08beb3e8980ef1a10607488de19dc243a45aa0db
Frédéric Dupérier <fduperier@gmail.com>
2014-12-12 20:34:51 +0100

    Adding UserGroups

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141212122130) do
+ActiveRecord::Schema.define(version: 20141212181046) do
+
+  create_table "user_groups", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+# Création de quelques User Groups
+user_group_jug = UserGroup.find_or_create_by!(name: "Geneva JUG")
+puts 'CREATED USER GROUPS: ' << user_group_jug.name
+
+user_group = UserGroup.find_or_create_by!(name: "GVA .NET User Group")
+puts 'CREATED USER GROUPS: ' << user_group.name
+
+user_group = UserGroup.find_or_create_by!(name: "Scala Romandie")
+puts 'CREATED USER GROUPS: ' << user_group.name
+
+user_group = UserGroup.find_or_create_by!(name: "Docker Geneva")
+puts 'CREATED USER GROUPS: ' << user_group.name
+
+user_group = UserGroup.find_or_create_by!(name: "Geneva MUG")
+puts 'CREATED USER GROUPS: ' << user_group.name
